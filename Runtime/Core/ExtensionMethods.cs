8e391d815eea7fcb6f134c2788d35afe19ac927e
seyedmoeinsaadati <saadatimoin@gmail.com>
2022-02-10 15:05:35 +0330

    add 2D rotate formulas

--- Runtime/Core/ExtensionMethods.cs
+++ Runtime/Core/ExtensionMethods.cs
@@ -91,6 +91,33 @@ namespace Moein.Core
                 Mathf.Abs((secondRangeMax - secondRangeMin) / (firstRangeMax - firstRangeMin)) + secondRangeMin;
         }
 
+        public static Vector3 RotateXY(this Vector3 vector, float angle, bool clockwise) //angle in radians
+        {
+            if (clockwise) angle = 2 * Mathf.PI - angle;
+
+            float xVal = vector.x * Mathf.Cos(angle) - vector.y * Mathf.Sin(angle);
+            float yVal = vector.x * Mathf.Sin(angle) + vector.y * Mathf.Cos(angle);
+            return new Vector3(xVal, yVal, 0);
+        }
+
+        public static Vector3 RotateXZ(this Vector3 vector, float angle, bool clockwise) //angle in radians
+        {
+            if (clockwise) angle = 2 * Mathf.PI - angle;
+
+            float xVal = vector.x * Mathf.Cos(angle) - vector.z * Mathf.Sin(angle);
+            float yVal = vector.x * Mathf.Sin(angle) + vector.z * Mathf.Cos(angle);
+            return new Vector3(xVal, 0, yVal);
+        }
+
+        public static Vector3 RotateYZ(this Vector3 vector, float angle, bool clockwise) // angle in radians
+        {
+            if (clockwise) angle = 2 * Mathf.PI - angle;
+
+            float xVal = vector.y * Mathf.Cos(angle) - vector.z * Mathf.Sin(angle);
+            float yVal = vector.y * Mathf.Sin(angle) + vector.z * Mathf.Cos(angle);
+            return new Vector3(0, xVal, yVal);
+        }
+
         #endregion
 
         #region Randomize
