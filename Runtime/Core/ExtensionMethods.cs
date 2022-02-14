cc870e40a3c94edf04f54c37d6c68637d5a3ba75
seyedmoeinsaadati <saadatimoin@gmail.com>
2022-02-14 21:28:52 +0330

    modify ExtensionMethods.cs

--- Runtime/Core/ExtensionMethods.cs
+++ Runtime/Core/ExtensionMethods.cs
@@ -91,27 +91,57 @@ namespace Moein.Core
                 Mathf.Abs((secondRangeMax - secondRangeMin) / (firstRangeMax - firstRangeMin)) + secondRangeMin;
         }
 
+        /// <summary>
+        /// Rotate 2D point (XY)
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="angle"></param>
+        /// <param name="clockwise"></param>
+        /// <returns></returns>
         public static Vector3 RotateXY(this Vector3 vector, float angle, bool clockwise) //angle in radians
         {
-            if (clockwise) angle = 2 * Mathf.PI - angle;
+            if (clockwise)
+            {
+                angle = 2 * Mathf.PI - angle;
+            }
 
             float xVal = vector.x * Mathf.Cos(angle) - vector.y * Mathf.Sin(angle);
             float yVal = vector.x * Mathf.Sin(angle) + vector.y * Mathf.Cos(angle);
             return new Vector3(xVal, yVal, 0);
         }
 
+        /// <summary>
+        /// Rotate 2D point (XZ)
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="angle"></param>
+        /// <param name="clockwise"></param>
+        /// <returns></returns>
         public static Vector3 RotateXZ(this Vector3 vector, float angle, bool clockwise) //angle in radians
         {
-            if (clockwise) angle = 2 * Mathf.PI - angle;
+            if (clockwise)
+            {
+                angle = 2 * Mathf.PI - angle;
+            }
 
             float xVal = vector.x * Mathf.Cos(angle) - vector.z * Mathf.Sin(angle);
             float yVal = vector.x * Mathf.Sin(angle) + vector.z * Mathf.Cos(angle);
             return new Vector3(xVal, 0, yVal);
         }
 
+        /// <summary>
+        /// Rotate 2D point (YZ)
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="angle"></param>
+        /// <param name="clockwise"></param>
+        /// <returns></returns>
         public static Vector3 RotateYZ(this Vector3 vector, float angle, bool clockwise) // angle in radians
         {
-            if (clockwise) angle = 2 * Mathf.PI - angle;
+            if (clockwise)
+            {
+                angle = 2 * Mathf.PI - angle;
+            }
 
             float xVal = vector.y * Mathf.Cos(angle) - vector.z * Mathf.Sin(angle);
             float yVal = vector.y * Mathf.Sin(angle) + vector.z * Mathf.Cos(angle);
@@ -141,6 +171,15 @@ namespace Moein.Core
             self.y = isAdditive ? self.y + Random.Range(min.y, max.y) : Random.Range(min.y, max.y);
             self.z = isAdditive ? self.z + Random.Range(min.z, max.z) : Random.Range(min.z, max.z);
             return self;
+        }
+
+        #endregion
+
+        #region Layer
+
+        public static bool Contains(this LayerMask self, int layer)
+        {
+            return ((int) Mathf.Pow(2, layer) & self) != 0;
         }
 
         #endregion
