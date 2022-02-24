6ccf2120347b2a9803136c14de02d8aee2424884
seyedmoeinsaadati <saadatimoin@gmail.com>
2022-02-24 08:59:44 +0330

    add SceneViewNavigation to control camera view in scene windows add lock camera rotation function add toggle camera projection

---
+++ Editor/Core/SceneViewNavigation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using static UnityEditor.SceneView;
+
+namespace Moein.Core
+{
+    public static class SceneViewNavigation
+    {
+        public static bool RotationLock
+        {
+            get => lastActiveSceneView.isRotationLocked;
+            set => lastActiveSceneView.isRotationLocked = value;
+        }
+
+        public static bool Orthographic
+        {
+            get => lastActiveSceneView.orthographic;
+            set => lastActiveSceneView.orthographic = value;
+        }
+
+        [MenuItem("Moein/View/Toggle Projection")]
+        public static void ToggleProjection()
+        {
+            if (lastActiveSceneView == null) return;
+            Orthographic = !Orthographic;
+            RepaintAll();
+        }
+
+        [MenuItem("Moein/View/Lock Rotation")]
+        public static void LockRotation()
+        {
+            if (lastActiveSceneView == null) return;
+            RotationLock = !RotationLock;
+            RepaintAll();
+        }
+    }
+}
