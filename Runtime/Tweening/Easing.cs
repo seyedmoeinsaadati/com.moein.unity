ebb84faad54b65d373cb353e77defe3ff20c6ca5
Seyed Moein Saadati <saadati.moin@gmail.com>
2022-06-02 23:08:55 +0430

    add tweening namespace add Easing.cs

---
+++ Runtime/Tweening/Easing.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Moein
+{
+    public class Easing
+    {
+        public enum Ease
+        {
+            InSin,
+            OutSin,
+            InOutSin,
+            InQuad,
+            OutQuad,
+            InOutQuad,
+            InCubic,
+            OutCubic,
+            InOutCubic,
+            InQuart,
+            OutQuart,
+            InOutQuart,
+            InQuint,
+            OutQuint,
+            InOutQuint,
+            InExpo,
+            OutExpo,
+            InOutExpo,
+            InCirc,
+            OutCirc,
+            InOutCirc,
+            InBack,
+            OutBack,
+            InOutBack,
+            InElastic,
+            OutElastic,
+            InOutElastic,
+            InBounce,
+            OutBounce,
+            InOutBounce
+        }
+
+        public static float Evaluate(float t)
+        {
+            return 0;
+        }
+    }
+}
