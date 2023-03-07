1731a4f874f90a1b1749a254096ce7f5616cb42c
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-03-07 22:07:15 +0330

    Add IAnimator inteface

---
+++ Runtime/Interfaces/IAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Moein.ComponentInterface
+{
+    public interface IAnimator
+    {
+        Animator Animator { get; }
+        float Speed { get; set; }
+        bool ApplyRootMotion { get; set; }
+        void SetTrigger(int id);
+        void SetTrigger(string name);
+        void SetInteger(int id, int value);
+        void SetInteger(string name, int value);
+        void SetFloat(int id, float value);
+        void SetFloat(string name, float value);
+        void SetFloat(int id, float value, float dampTime, float deltaTime);
+        void SetFloat(string name, float value, float dampTime, float deltaTime);
+        void SetBool(int id, bool value);
+        void SetBool(string name, bool value);
+    }
+}
