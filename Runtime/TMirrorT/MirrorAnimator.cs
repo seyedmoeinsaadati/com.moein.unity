0b3b5e8c50fae785e44bef33c860307685f74ff0
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-03-08 17:36:47 +0330

    Add MirrorAnimator, SyncAnimator to Mirror animators

---
+++ Runtime/TMirrorT/MirrorAnimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Moein.Mirror
+{
+    public class MirrorAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private SyncAnimator target;
+
+        private void Reset()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void SetTarget(SyncAnimator target)
+        {
+            if (this.target != null)
+                this.target.Remove(animator);
+            this.target = target;
+            target.Add(animator);
+        }
+
+        private void OnEnable()
+        {
+            target.Add(animator);
+        }
+
+        private void OnDisable()
+        {
+            target.Remove(animator);
+        }
+    }
+}
---
+++ Runtime/TMirrorT/SyncAnimator.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections.Generic;
+using log4net.Util;
+using UnityEngine;
+
+namespace Moein.Mirror
+{
+    public class SyncAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        private List<Animator> syncAnimators = new List<Animator>();
+
+        public Animator Animator => _animator;
+
+        public float Speed
+        {
+            get { return _animator.speed; }
+            set
+            {
+                _animator.speed = value;
+
+                if (syncAnimators.Count > 0)
+                {
+                    for (int i = 0; i < syncAnimators.Count; i++)
+                    {
+                        syncAnimators[i].speed = value;
+                    }
+                }
+            }
+        }
+
+        public bool ApplyRootMotion
+        {
+            get { return _animator.applyRootMotion; }
+            set
+            {
+                _animator.applyRootMotion = value;
+
+                if (syncAnimators.Count > 0)
+                {
+                    for (int i = 0; i < syncAnimators.Count; i++)
+                    {
+                        syncAnimators[i].applyRootMotion = value;
+                    }
+                }
+            }
+        }
+
+        public void SetTrigger(int id)
+        {
+            _animator.SetTrigger(id);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetTrigger(id);
+                }
+            }
+        }
+
+        public void SetTrigger(string name)
+        {
+            _animator.SetTrigger(name);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetTrigger(name);
+                }
+            }
+        }
+
+        public void SetInteger(int id, int value)
+        {
+            _animator.SetInteger(id, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetInteger(id, value);
+                }
+            }
+        }
+
+        public void SetInteger(string name, int value)
+        {
+            _animator.SetInteger(name, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetInteger(name, value);
+                }
+            }
+        }
+
+        public void SetFloat(int id, float value)
+        {
+            _animator.SetFloat(id, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetFloat(id, value);
+                }
+            }
+        }
+
+        public void SetFloat(string name, float value)
+        {
+            _animator.SetFloat(name, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetFloat(name, value);
+                }
+            }
+        }
+
+        public void SetFloat(int id, float value, float dampTime, float deltaTime)
+        {
+            _animator.SetFloat(name, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetFloat(id, value, dampTime, deltaTime);
+                }
+            }
+        }
+
+        public void SetFloat(string name, float value, float dampTime, float deltaTime)
+        {
+            _animator.SetFloat(name, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetFloat(name, value, dampTime, deltaTime);
+                }
+            }
+        }
+
+        public void SetBool(int id, bool value)
+        {
+            _animator.SetBool(id, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetBool(id, value);
+                }
+            }
+        }
+
+        public void SetBool(string name, bool value)
+        {
+            _animator.SetBool(name, value);
+
+            if (syncAnimators.Count > 0)
+            {
+                for (int i = 0; i < syncAnimators.Count; i++)
+                {
+                    syncAnimators[i].SetBool(name, value);
+                }
+            }
+        }
+
+
+        private void Reset()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Add(Animator animator)
+        {
+            if (animator == null) return;
+            if (syncAnimators.Contains(animator) == false)
+            {
+                syncAnimators.Add(animator);
+            }
+        }
+
+        public void Remove(Animator animator)
+        {
+            if (animator == null) return;
+            if (syncAnimators.Contains(animator))
+            {
+                syncAnimators.Remove(animator);
+            }
+        }
+    }
+}
