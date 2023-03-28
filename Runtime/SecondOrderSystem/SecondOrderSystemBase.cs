8d358e7ea0c62eef514af6a8fa93ebc10f6506c0
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-03-28 18:25:38 +0330

    Add SecondOrderSystem scripts

---
+++ Runtime/SecondOrderSystem/SecondOrderSystemBase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Moein.Core
+{
+    public abstract class SecondOrderSystemBase<T>
+    {
+        public const float PI = Mathf.PI;
+
+        protected T xp;
+        protected T y, yd;
+
+        protected float k1, k2, k3;
+        protected float T_crit;
+        protected float w;
+
+        protected SecondOrderSystemBase(T initializeState, float f, float z, float r)
+        {
+            w = (2 * PI * f);
+            k1 = z / (PI * f);
+            k2 = 1 / (w * w);
+            k3 = r * z / w;
+
+            T_crit = .8f * (Mathf.Sqrt(4 * k2 + k1 * k1) - k1);
+
+            xp = initializeState;
+            y = initializeState;
+            yd = default;
+        }
+
+        /// <param name="x">currentState</param>
+        /// <param name="xd">derivation of currentState </param>
+        public abstract T Update(float deltaTime, T x, T xd = default);
+    }
+}
---
+++ Runtime/SecondOrderSystem/SecondOrderSystem_Vector3.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Moein.Core
+{
+    public class SecondOrderSystem_Vector3 : SecondOrderSystemBase<Vector3>
+    {
+        public SecondOrderSystem_Vector3(Vector3 initializeState, float f, float z, float r) : base(initializeState, f, z, r)
+        {
+        }
+
+        public override Vector3 Update(float T, Vector3 x, Vector3 xd = default)
+        {
+            if (xd == default)
+            {
+                xd = (x - xp) / T;
+                xp = x;
+            }
+
+            float kStable;
+            kStable = Mathf.Max(k2, T * T / 2 + T * k1 / 2, T * k1);
+
+            int iterations = (int)Mathf.Ceil(T / T_crit);
+            T /= iterations;
+            for (int i = 0; i < iterations; i++)
+            {
+                y += T * yd;
+                yd += T * (x + k3 * xd - y - k1 * yd) / kStable;
+            }
+
+            return y;
+        }
+    }
+}
